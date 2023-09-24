37576298508f7139b95e34d67011b23a65a7791c
Alex <alex@devaccent.com>
2023-09-24 16:40:01 +0300

    Add events for deposit, withdraw and transfer

--- src/events.rs
+++ src/events.rs
@@ -3,6 +3,35 @@ multiversx_sc::derive_imports!();
 
 #[multiversx_sc::module]
 pub trait EventsModule {
+    #[event("deposit")]
+    fn deposit_event(
+        &self,
+        #[indexed] address: &ManagedAddress,
+        #[indexed] token_identifier: &EgldOrEsdtTokenIdentifier,
+        #[indexed] token_nonce: u64,
+        #[indexed] amount: &BigUint,
+    );
+
+    #[event("withdraw")]
+    fn withdraw_event(
+        &self,
+        #[indexed] address: &ManagedAddress,
+        #[indexed] token_identifier: &EgldOrEsdtTokenIdentifier,
+        #[indexed] token_nonce: u64,
+        #[indexed] amount: &BigUint,
+    );
+
+    #[event("transfer")]
+    fn transfer_event(
+        &self,
+        #[indexed] sender: &ManagedAddress,
+        #[indexed] receiver: &ManagedAddress,
+        #[indexed] token_identifier: &EgldOrEsdtTokenIdentifier,
+        #[indexed] token_nonce: u64,
+        #[indexed] amount: &BigUint,
+        #[indexed] is_internal: bool,
+    );
+
     #[event("createPaymentAgreement")]
     fn create_payment_agreement(
         &self,
@@ -10,6 +39,11 @@ pub trait EventsModule {
 
     #[event("cancelPaymentAgreement")]
     fn cancel_payment_agreement(
+        &self,
+    );
+
+    #[event("chargePaymentAgreement")]
+    fn charge_payment_agreement(
         &self,
     );
 }
