9015a6cd07de0725b57afa19a7be9de3a6bc8b24
Alex <alex@devaccent.com>
2023-10-18 01:20:52 +0300

    Add function to construct agreement amount

--- src/agreement_amount.rs
+++ src/agreement_amount.rs
@@ -1,13 +1,34 @@
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
-use crate::types::{Agreement, AgreementAmountType, Amount};
+use crate::types::{AgreementType, AgreementAmountType, Amount};
 
 #[multiversx_sc::module]
 pub trait AgreementAmountModule:
     crate::storage::StorageModule +
     crate::validation::ValidationModule
 {
+    fn construct_agreement_amount(
+        &self,
+        agreement_type: AgreementType,
+        amount_type: AgreementAmountType,
+        amount: Option<Amount<Self::Api>>
+    ) -> Amount<Self::Api> {
+        match agreement_type {
+            // TODO: We allow only RecurringPayoutToReceive for the xDay Hackathon
+            AgreementType::RecurringPayoutToReceive => {
+                match amount_type {
+                    // TODO: We allow only FixedAmount for the xDay Hackathon
+                    AgreementAmountType::FixedAmount => {
+                        self.construct_defined_amount(amount_type, amount)
+                    },
+                    _ => panic!("Invalid amount type")
+                }
+            },
+            _ => panic!("Invalid agreement type")
+        }
+    }
+
     fn construct_defined_amount(&self, amount_type: AgreementAmountType, amount: Option<Amount<Self::Api>>) -> Amount<Self::Api> {
         match amount_type {
             AgreementAmountType::FixedAmount |
@@ -27,60 +48,40 @@ pub trait AgreementAmountModule:
     }
 
     fn construct_fixed_amount(&self, amount: Option<Amount<Self::Api>>) -> Amount<Self::Api> {
-        let amountStruct = amount.expect("Amount should be provided for this agreement type");
-
-        let fixed_amount = amountStruct.fixed_amount.expect("Fixed amount should be provided");
+        let amount_struct = amount.expect("Amount should be provided for this agreement type");
+        let fixed_amount = amount_struct.fixed_amount.expect("Fixed amount should be provided");
 
         Amount {
-            fixed_amount: amountStruct.fixed_amount,
+            fixed_amount: Some(fixed_amount),
             minimum_amount: None,
             maximum_amount: None,
         }
     }
 
     fn construct_bounded_amount(&self, amount: Option<Amount<Self::Api>>) -> Amount<Self::Api> {
-        let amountStruct = amount.expect("Amount should be provided for this agreement type");
-
-        let minimum_amount = amountStruct.minimum_amount.expect("Minimum amount should be provided");
-        let maximum_amount = amountStruct.maximum_amount.expect("Maximum amount should be provided");
+        let amount_struct = amount.expect("Amount should be provided for this agreement type");
+        let minimum_amount = amount_struct.minimum_amount.expect("Minimum amount should be provided");
+        let maximum_amount = amount_struct.maximum_amount.expect("Maximum amount should be provided");
 
         Amount {
             fixed_amount: None,
-            minimum_amount: amountStruct.minimum_amount,
-            maximum_amount: amountStruct.maximum_amount,
+            minimum_amount: Some(minimum_amount),
+            maximum_amount: Some(maximum_amount),
         }
     }
 
-    fn get_amount_agreed_by_parties(&self, agreement: Agreement<Self::Api>, address: ManagedAddress<Self::Api>) -> Amount<Self::Api> {
-        match agreement.amount_type {
+    fn get_amount_agreed_by_parties(&self, agreement_id: u64, amount_type: AgreementAmountType, address: &ManagedAddress<Self::Api>) -> Amount<Self::Api> {
+        match amount_type {
             AgreementAmountType::FixedAmount |
             AgreementAmountType::BoundedAmount => {
-                agreement.amount.unwrap()
+                self.agreement_amount(agreement_id).get()
             },
 
             AgreementAmountType::SenderDefinedFixedAmount |
             AgreementAmountType::CreatorDefinedFixedAmountPerReceiver |
             AgreementAmountType::SenderDefinedBoundedAmount |
             AgreementAmountType::CreatorDefinedBoundedAmountPerReceiver => {
-                self.agreement_defined_amount_per_account(agreement.id, &address).get()
-            },
-
-            _ => panic!("Invalid agreement amount type")
-        }
-    }
-
-    fn is_amount_in_agreed_bounds(&self, amount_type: AgreementAmountType, agreed_amount: Amount<Self::Api>, amount: BigUint) -> bool {
-        match amount_type {
-            AgreementAmountType::FixedAmount |
-            AgreementAmountType::SenderDefinedFixedAmount |
-            AgreementAmountType::CreatorDefinedFixedAmountPerReceiver => {
-                amount == agreed_amount.fixed_amount.unwrap()
-            },
-
-            AgreementAmountType::BoundedAmount |
-            AgreementAmountType::SenderDefinedBoundedAmount |
-            AgreementAmountType::CreatorDefinedBoundedAmountPerReceiver => {
-                amount <= agreed_amount.maximum_amount.unwrap() && amount >= agreed_amount.minimum_amount.unwrap()
+                self.agreement_defined_amount_per_account(agreement_id, address).get()
             },
 
             _ => panic!("Invalid agreement amount type")
