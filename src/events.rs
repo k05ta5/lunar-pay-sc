0aa780e54d9dd737931ab8b1b5d66f918b3ecd6e
Alex <alex@devaccent.com>
2023-10-18 01:16:48 +0300

    Update events

--- src/events.rs
+++ src/events.rs
@@ -1,6 +1,8 @@
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
+use crate::types::{AgreementAmountType, AgreementType, Amount};
+
 #[multiversx_sc::module]
 pub trait EventsModule {
     #[event("deposit")]
@@ -33,17 +35,26 @@ pub trait EventsModule {
     );
 
     #[event("createPaymentAgreement")]
-    fn create_payment_agreement(
+    fn create_payment_agreement_event(
         &self,
+        #[indexed] agreement_id: u64,
+        #[indexed] owner: &ManagedAddress,
+        #[indexed] token_nonce: u64,
+        #[indexed] token_identifier: &EgldOrEsdtTokenIdentifier,
+        #[indexed] frequency: u64,
+        #[indexed] time_created: u64,
+        #[indexed] agreement_type: AgreementType,
+        #[indexed] amount_type: AgreementAmountType,
+        #[indexed] amount: Option<Amount<Self::Api>>,
     );
 
     #[event("cancelPaymentAgreement")]
-    fn cancel_payment_agreement(
+    fn cancel_payment_agreement_event(
         &self,
     );
 
     #[event("chargePaymentAgreement")]
-    fn charge_payment_agreement(
+    fn charge_payment_agreement_event(
         &self,
     );
 }
