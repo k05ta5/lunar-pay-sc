24a6f580178d44807bdd4852114ab5d8272a79bc
Alex <alex@devaccent.com>
2024-01-19 17:45:37 +0200

    Add metadata to sign payment agreement

--- src/events.rs
+++ src/events.rs
@@ -67,6 +67,7 @@ pub trait EventsModule {
         #[indexed] agreement_id: u64,
         #[indexed] member: &ManagedAddress,
         #[indexed] signed_at: u64,
+        metadata: Option<ManagedBuffer>,
     );
 
     #[event("cancelPaymentAgreement")]
